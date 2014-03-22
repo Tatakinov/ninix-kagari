50696d3cb1d8f1e17d899e19d89d707fa5e240af
Shyouzou Sugitani <shy@users.sourceforge.jp>
2014-03-22 19:28:50 +0900

    update on the TEST of pix.rb

--- lib/ninix/pix.rb
+++ lib/ninix/pix.rb
@@ -348,30 +348,27 @@ module Pix
     return left, top, width, height
   end
 
-end
-
-
-module TEST
-
-  class Main < Pix::TransparentWindow
+  class TEST
 
     def initialize(path)
-      super()
-      signal_connect('destroy') do
+      @win = Pix::TransparentWindow.new
+      @win.signal_connect('destroy') do
         Gtk.main_quit
       end
-      @darea.signal_connect('draw') do |w, cr|
+      @win.darea.signal_connect('draw') do |w, cr|
         expose_cb(w, cr)
       end
       @surface = Pix.create_surface_from_file(path, true, true)
-      set_default_size(@surface.width, @surface.height)
+      @win.set_default_size(@surface.width, @surface.height)
+      @win.show_all
+      Gtk.main
     end
 
     def expose_cb(widget, cr)
       cr.set_source(@surface, 0, 0)
       cr.set_operator(Cairo::OPERATOR_SOURCE)
       cr.paint
-      region = Cairo::Region.new()#cr.target)
+      region = Cairo::Region.new()
       data = @surface.data
       for i in 0..(data.size / 4 - 1)
         if (data[i * 4 + 3].ord) != 0
@@ -380,14 +377,12 @@ module TEST
           region.union!(x, y, 1, 1)
         end
       end
-      input_shape_combine_region(region)
+      @win.input_shape_combine_region(region)
     end
   end
 end
 
+
 $:.unshift(File.dirname(__FILE__))
 
-main = TEST::Main.new(ARGV.shift)
-main.show_all
-
-Gtk.main
+Pix::TEST.new(ARGV.shift)
