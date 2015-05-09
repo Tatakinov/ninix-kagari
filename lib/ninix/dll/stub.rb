fd13ada7ee59cd8bb33ac199423bd2ff73b039aa
Shyouzou Sugitani <shy@users.sourceforge.jp>
2015-05-09 22:14:35 +0900

    add SHIORI stub

---
+++ lib/ninix/dll/stub.rb
@@ -0,0 +1,22 @@
+module Stub
+
+  class Shiori
+
+    def initialize(dll_name)
+    end
+
+    def find(dir, dll)
+      return 300
+    end
+
+    def load(dir)
+    end
+
+    def unload()
+    end
+
+    def request(req_str)
+      return ""
+    end
+  end
+end
