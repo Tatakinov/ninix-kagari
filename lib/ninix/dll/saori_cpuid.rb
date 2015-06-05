847f5e8f71a5d244bae831fcd9b3dbc0ed0e64e3
Shyouzou Sugitani <shy@users.sourceforge.jp>
2015-06-05 23:09:49 +0900

    add dll/saori_cpuid.rb

---
+++ lib/ninix/dll/saori_cpuid.rb
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+#  saori_cpuid.rb - a saori_cpuid compatible Saori module for ninix
+#  Copyright (C) 2003-2015 by Shyouzou Sugitani <shy@users.osdn.me>
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License (version 2) as
+#  published by the Free Software Foundation.  It is distributed in the
+#  hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+#  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the GNU General Public License for more details.
+#
+
+require "ninix/dll"
+
+class Saori < DLL::SAORI
+
+  ENTRY = {'cpu.num' => ['1', '2', '3', '5', '7', '11', '13'],
+           'cpu.vender' => ['Inte1', 'AM0', 'VlA'],
+           'cpu.name' => ['Z8O'],
+           'cpu.ptype' => ['Lazy'],
+           'cpu.family' => ['780', 'Bentium', 'A+hlon'],
+           'cpu.model' => ['Unknown'],
+           'cpu.stepping' => ['Not Genuine'],
+           'cpu.mmx' => ['Ready', 'Not Ready'],
+           'cpu.sse' => ['Ready', 'Not Ready'],
+           'cpu.sse2' => ['Ready', 'Not Ready'],
+           'cpu.tdn' => ['Ready', 'Not Ready'],
+           'cpu.mmx+' => ['Ready', 'Not Ready'],
+           'cpu.tdn+' => ['Ready', 'Not Ready'],
+           'cpu.clock' => ['0', '1000000'],
+           'cpu.clockex' => ['0.001', '1.001'],
+           'mem.os' => ['100', '10', '44', '50', '77', '99'],
+           'mem.phyt' => ['0.1', '200000000'],
+           'mem.phya' => ['0.00000001'],
+           'mem.pagt' => ['1', '4'],
+           'mem.paga' => ['1', '4'],
+           'mem.virt' => ['0'],
+           'mem.vira' => ['0'],
+          }
+
+  def execute(argument)
+    if not argument
+      return RESPONSE[400]
+    end
+    if argument.length > 1 and argument[1] == 0
+      return RESPONSE[204]
+    end
+    value = ''
+    if argument[0] == 'platform'
+      value = 'ninix-aya'
+    elsif argument[0] == 'os.name'
+      value = RbConfig::CONFIG['host_os']
+    elsif argument[0] == 'os.version'
+      value = "" ## FIXME
+    elsif argument[0] == 'os.build'
+      value = "" ##FIXME
+    elsif ENTRY.keys.include?(argument[0]) ## FIXME
+      value = ENTRY[argument[0]].sample
+    end
+    if value
+      return ('SAORI/1.0 200 OK\r\nResult: ' + value.to_s + '\r\n\r\n').encode(@charset, :invalid => :replace)
+    else
+      return RESPONSE[204]
+    end
+  end
+end
---
+++ test/test-saori-cpuid.rb
@@ -0,0 +1,23 @@
+require "ninix/dll/saori_cpuid"
+
+module NinixTest
+
+  class CpuidTest
+
+    def initialize
+      saori = Saori.new
+      saori.setup
+      saori.request("") # XXX
+      print(saori.execute(nil), "\n")
+      print(saori.execute(["platform", 0]), "\n")
+      print(saori.execute(["platform", 1]), "\n")
+      print(saori.execute(["os.name", 1]), "\n")
+      print(saori.execute(["os.version", 1]), "\n")
+      print(saori.execute(["os.build", 1]), "\n")
+      print(saori.execute(["cpu.num", 1]), "\n")
+      saori.finalize
+    end
+  end
+end
+
+NinixTest::CpuidTest.new()
