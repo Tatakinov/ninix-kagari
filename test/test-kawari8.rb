6adcb45c6ee5f60c0cf1f36bfc01d36a990ed548
Shyouzou Sugitani <shy@users.sourceforge.jp>
2016-02-02 22:47:08 +0900

    update test-kawari8.rb

--- test/test-kawari8.rb
+++ test/test-kawari8.rb
@@ -11,9 +11,20 @@ end
 
 path = ARGV[0].to_s.encode("Shift_JIS")
 #print path, "\n"
-Test.load(path, path.bytesize)
+path_ptr = Fiddle::Pointer.malloc(
+  path.bytesize + 1,
+  freefunc=nil # Kawari8 will free this pointer
+)
+Test.load(path_ptr, path.bytesize)
 test_text = "NOTIFY SHIORI/3.0\r\nCharset: Shift_JIS\r\nID: TEST                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           日本語\r\n\r\n".encode("Shift_JIS")
-test_len = [test_text.bytesize].pack("q")
-result = Test.request(test_text, test_len)
-print result.to_s.force_encoding("Shift_JIS").encode("UTF-8")
+test_len = [test_text.bytesize].pack("l!")
+test_ptr = Fiddle::Pointer.malloc(
+  test_text.bytesize + 1,
+  freefunc=nil # Kawari8 will free this pointer
+)
+test_ptr[0, test_text.bytesize] = test_text
+result = Test.request(test_ptr, test_len)
+test_len, = test_len.unpack("l!")
+#print result.methods.sort, "\n"
+print result[0, test_len].to_s.force_encoding("Shift_JIS").encode("UTF-8")
 Test.unload()
