e167cf7622c7cca4c3c6386d82c873719c270c73
Shyouzou Sugitani <shy@users.sourceforge.jp>
2016-02-04 22:08:22 +0900

    update test-kawari8.rb

--- test/test-kawari8.rb
+++ test/test-kawari8.rb
@@ -4,27 +4,26 @@ require "fiddle/import"
 module Test
   extend Fiddle::Importer
   dlload "libshiori.so" #"_kawari8.so"
-  extern "int load(const char *, long)"
-  extern "char *request(const char *, long *)"
-  extern "int unload()"
+  extern "int so_library_init()"
+  extern "int so_library_cleanup()"
+  extern "unsigned int so_create(const char *, long)"
+  extern "int so_dispose(unsigned int)"
+  extern "const char *so_request(unsigned int, const char *, long *)"
+  extern "void so_free(unsigned int, const char *)"
 end
+
+Test.so_library_init()
 
 path = ARGV[0].to_s.encode("Shift_JIS")
 #print path, "\n"
-path_ptr = Fiddle::Pointer.malloc(
-  path.bytesize + 1,
-  freefunc=nil # Kawari8 will free this pointer
-)
-Test.load(path_ptr, path.bytesize)
+handle = Test.so_create(path, path.bytesize)
 test_text = "NOTIFY SHIORI/3.0\r\nCharset: Shift_JIS\r\nID: TEST                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           日本語\r\n\r\n".encode("Shift_JIS")
 test_len = [test_text.bytesize].pack("l!")
-test_ptr = Fiddle::Pointer.malloc(
-  test_text.bytesize + 1,
-  freefunc=nil # Kawari8 will free this pointer
-)
-test_ptr[0, test_text.bytesize] = test_text
-result = Test.request(test_ptr, test_len)
+result = Test.so_request(handle, test_text, test_len)
 test_len, = test_len.unpack("l!")
 #print result.methods.sort, "\n"
 print result[0, test_len].to_s.force_encoding("Shift_JIS").encode("UTF-8")
-Test.unload()
+Test.so_free(handle, result)
+
+Test.so_dispose(handle)
+Test.so_library_cleanup()
