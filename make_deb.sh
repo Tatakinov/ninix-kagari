98d5fcad93089659fe1bd9596e903529399cff6e
Tatakinov <tatakinov@gmail.com>
2024-09-23 21:24:12 +0900

    deb作成用のファイル群を追加。

---
+++ make_deb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+WORKDIR=work
+RUBY=ruby
+BINDIR=usr/games
+LIBDIR=usr/lib/games/ninix-kagari
+
+SOPATH=/usr/lib/games/ninix-kagari/kawari8:/usr/lib/games/ninix-kagari/yaya:/usr/lib/games/ninix-kagari/kagari
+
+SAORIPATH=/usr/lib/games/ninix-kagari/saori
+
+VERSION=$(./print_version.rb)
+
+mkdir -p ${WORKDIR}/${BINDIR} ${WORKDIR}/${LIBDIR}
+cp -r debian ${WORKDIR}/DEBIAN
+sed -e "s,@ruby,${RUBY},g" -e "s,@libdir,/${LIBDIR},g" -e "s,@so_path,${SOPATH},g" -e "s,@saori_path,${SAORIPATH},g" < bin/ninix.in > ${WORKDIR}/usr/games/ninix
+cp -r lib/* ${WORKDIR}/usr/lib/games/ninix-kagari/
+
+pushd ${WORKDIR}
+find usr -type f -exec md5sum {} \+ > DEBIAN/md5sums
+INSTALLED_SIZE=$(du -sk usr | cut -f 1)
+sed -i -e "s/@installed_size/${INSTALLED_SIZE}/g" -e "s/@version/${VERSION}/g" DEBIAN/control
+popd
+fakeroot dpkg-deb --build ${WORKDIR} .
---
+++ print_version.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+require_relative "lib/ninix/version"
+
+print Version.NUMBER
