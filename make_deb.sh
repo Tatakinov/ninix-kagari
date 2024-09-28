33bc8630480cad588dd671c47d6064a6e1d7c2f5
Tatakinov <tatakinov@gmail.com>
2024-09-28 17:38:37 +0900

    post系は使わないので削除。saoriのディレクトリを指定するように。

--- make_deb.sh
+++ make_deb.sh
@@ -3,16 +3,23 @@ WORKDIR=work
 RUBY=ruby
 BINDIR=usr/games
 LIBDIR=usr/lib/games/ninix-kagari
+LOCALEDIR=usr/share/locale
 
 SAORIPATH=/usr/lib/games/ninix-kagari/saori
 SOPATH=${SAORIPATH}:/usr/lib/games/ninix-kagari/kawari8:/usr/lib/games/ninix-kagari/yaya:/usr/lib/games/ninix-kagari/kagari
 
 VERSION=$(./print_version.rb)
 
-mkdir -p ${WORKDIR}/${BINDIR} ${WORKDIR}/${LIBDIR}
+mkdir -p ${WORKDIR}/${BINDIR} ${WORKDIR}/${LIBDIR} ${WORKDIR}/${LOCALEDIR}
 cp -r debian ${WORKDIR}/DEBIAN
 sed -e "s,@ruby,${RUBY},g" -e "s,@libdir,/${LIBDIR},g" -e "s,@so_path,${SOPATH},g" -e "s,@saori_path,${SAORIPATH},g" < bin/ninix.in > ${WORKDIR}/usr/games/ninix
 cp -r lib/* ${WORKDIR}/usr/lib/games/ninix-kagari/
+pushd po
+for i in *; do
+    mkdir -p ../${WORKDIR}/${LOCALEDIR}/${i}/LC_MESSAGES
+    msgfmt ${i}/ninix-kagari.po -o ../${WORKDIR}/${LOCALEDIR}/${i}/LC_MESSAGES/ninix-kagari.mo
+done
+popd
 
 pushd ${WORKDIR}
 find usr -type f -exec md5sum {} \+ > DEBIAN/md5sums
