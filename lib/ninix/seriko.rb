edbe5ff653dfcd52ef59027bd0f62b5d19af9fa6
Tatakinov <tatakinov@gmail.com>
2025-07-21 14:50:20 +0900

    (C)を追記。

--- lib/ninix/seriko.rb
+++ lib/ninix/seriko.rb
@@ -3,7 +3,7 @@
 #  Copyright (C) 2002 by Tamito KAJIYAMA
 #  Copyright (C) 2002, 2003 by MATSUMURA Namihiko <nie@counterghost.net>
 #  Copyright (C) 2002-2019 by Shyouzou Sugitani <shy@users.osdn.me>
-#  Copyright (C) 2024 by Tatakinov
+#  Copyright (C) 2024, 2025 by Tatakinov
 #
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License (version 2) as
