de0ca02cb60d39df8d326ad06745416ea0027f9f
Tatakinov <tatakinov@gmail.com>
2026-07-30 22:54:12 +0900

    deprecatedだったのを改善した。

--- lib/ninix/script_log.rb
+++ lib/ninix/script_log.rb
@@ -18,27 +18,87 @@ module ScriptLog
 
   bindtextdomain('ninix-kagari')
 
+  class Item < GLib::Object
+    type_register
+    attr_accessor :time, :ghost_name, :script
+
+    def initialize(time, ghost_name, script)
+      super()
+      @time = time
+      @ghost_name = ghost_name
+      @script = script
+    end
+
+=begin
+    install_property(GLib::Param::String.new('time', 'time', 'Time', '', GLib::Param::READWRITE))
+    install_property(GLib::Param::String.new('ghost_name', 'ghost_name', 'Ghost Name', '', GLib::Param::READWRITE))
+    install_property(GLib::Param::String.new('script', 'script', 'Script', '', GLib::Param::READWRITE))
+=end
+  end
+
   class Window < Gtk::Window
     def initialize
       super
+      # FIXME gettext
       set_default_size(640, 360)
       box = Gtk::Box.new(:vertical, 0)
       set_child(box)
-      scroll = Gtk::ScrolledWindow.new
-      scroll.set_policy(:automatic, :automatic)
-      #box.pack_start(scroll, expand: true, fill: true, padding: 0)
-      box.append(scroll)
-      @model = Gtk::ListStore.new(String, String, String)
-      tree = Gtk::TreeView.new(@model)
-      tree.selection.set_mode(:single)
-      # FIXME gettext
-      column = Gtk::TreeViewColumn.new('Time', Gtk::CellRendererText.new, { text: 0 })
-      tree.append_column(column)
-      column = Gtk::TreeViewColumn.new('Ghost name', Gtk::CellRendererText.new, { text: 1 })
-      tree.append_column(column)
-      column = Gtk::TreeViewColumn.new('Script', Gtk::CellRendererText.new, { text: 2 })
-      tree.append_column(column)
-      scroll.set_child(tree)
+      @scroll = Gtk::ScrolledWindow.new
+      @scroll.set_policy(:automatic, :automatic)
+      @scroll.set_vexpand(true)
+      box.append(@scroll)
+      @model = Gio::ListStore.new(Item.gtype)
+      selection = Gtk::SingleSelection.new(@model)
+      column_view = Gtk::ColumnView.new(selection)
+
+      factory1 = Gtk::SignalListItemFactory.new
+      factory1.signal_connect('setup') do |f, list_item|
+        label = Gtk::Label.new
+        label.halign = :start
+        label.hexpand = true
+        list_item.child = label
+        p [:debug, :time]
+      end
+      factory1.signal_connect('bind') do |f, list_item|
+        label = list_item.child
+        item = list_item.item
+        label.text = item.time
+      end
+      column = Gtk::ColumnViewColumn.new('Time', factory1)
+      column_view.append_column(column)
+
+      factory2 = Gtk::SignalListItemFactory.new
+      factory2.signal_connect('setup') do |f, list_item|
+        label = Gtk::Label.new
+        label.halign = :start
+        label.hexpand = true
+        list_item.child = label
+        p [:debug, :name]
+      end
+      factory2.signal_connect('bind') do |f, list_item|
+        label = list_item.child
+        item = list_item.item
+        label.text = item.ghost_name
+      end
+      column = Gtk::ColumnViewColumn.new('Ghost Name', factory2)
+      column_view.append_column(column)
+
+      factory3 = Gtk::SignalListItemFactory.new
+      factory3.signal_connect('setup') do |f, list_item|
+        label = Gtk::Label.new
+        label.halign = :start
+        label.hexpand = true
+        list_item.child = label
+      end
+      factory3.signal_connect('bind') do |f, list_item|
+        label = list_item.child
+        item = list_item.item
+        label.text = item.script
+      end
+      column = Gtk::ColumnViewColumn.new('Script', factory3)
+      column_view.append_column(column)
+
+      @scroll.set_child(column_view)
       signal_connect('close-request') do |w, e|
         w.hide
       end
@@ -46,10 +106,7 @@ module ScriptLog
     end
 
     def append_data(name, script)
-      i = @model.prepend
-      i[0] = Time.now.to_s
-      i[1] = name
-      i[2] = script
+      @model.insert(0, Item.new(Time.now.to_s, name, script))
     end
   end
 end
